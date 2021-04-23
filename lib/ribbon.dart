3a60f66c2791adf693a6fc678c0422e03990512d
tduval <duval.thomas01@gmail.com>
2021-04-23 12:23:24 +0200

    Sound null safety update
    Update ribbon.dart for compatibility with sound null safety in flutter 2.x and dart >2.12.
    Make variable nullable ("?") or late initialized ("late").
    Also rename variable 'sum_x' to 'sumX' to be compliant with lowerCamelCase.

--- lib/ribbon.dart
+++ lib/ribbon.dart
@@ -4,13 +4,10 @@ import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
 const TextStyle _kTextStyle = TextStyle(
-  color: Color(0xFFFFFFFF),
-  fontSize: 12,
-  fontWeight: FontWeight.w900,
-  height: 1.0
-);
-
-
+    color: Color(0xFFFFFFFF),
+    fontSize: 12,
+    fontWeight: FontWeight.w900,
+    height: 1.0);
 
 enum RibbonLocation {
   topStart,
@@ -28,14 +25,14 @@ class Ribbon extends StatelessWidget {
   final RibbonLocation location;
   final Widget child;
   const Ribbon(
-      {Key key,
-      @required this.nearLength,
-      @required this.farLength,
-      @required this.title,
+      {Key? key,
+      required this.nearLength,
+      required this.farLength,
+      required this.title,
       this.titleStyle = _kTextStyle,
       this.color = Colors.white,
       this.location = RibbonLocation.topStart,
-      this.child})
+      required this.child})
       : super(key: key);
 
   @override
@@ -61,25 +58,25 @@ class _RibbonPainter extends CustomPainter {
   final TextStyle titleStyle;
   final RibbonLocation location;
   bool initialized = false;
-  TextPainter textPainter;
-  Paint paintRibbon;
-  Path pathRibbon;
-  double rotateRibbon;
-  Offset offsetRibbon;
-  Offset offsetTitle;
-  Paint paintShadow;
+  late TextPainter textPainter;
+  late Paint paintRibbon;
+  late Path pathRibbon;
+  late double rotateRibbon;
+  late Offset offsetRibbon;
+  late Offset offsetTitle;
+  late Paint paintShadow;
 
- static const BoxShadow _shadow = BoxShadow(
+  static const BoxShadow _shadow = BoxShadow(
     color: Color(0x7F000000),
     blurRadius: 6.0,
   );
   _RibbonPainter(
-      {this.nearLength,
-      this.farLength,
-      this.title,
-      this.titleStyle,
-      this.color,
-      this.location});
+      {required this.nearLength,
+      required this.farLength,
+      required this.title,
+      required this.titleStyle,
+      required this.color,
+      required this.location});
   @override
   void paint(Canvas canvas, Size size) {
     if (!initialized) _initializ(size);
@@ -87,10 +84,10 @@ class _RibbonPainter extends CustomPainter {
     canvas
       ..drawShadow(pathRibbon, Color(0x7F000000), 2.0, true)
       ..drawPath(pathRibbon, paintRibbon)
-    // canvas
+      // canvas
       ..translate(offsetRibbon.dx, offsetRibbon.dy)
       ..rotate(rotateRibbon);
-      // ..clipPath(pathRibbon);
+    // ..clipPath(pathRibbon);
     textPainter.paint(canvas, offsetTitle);
   }
 
@@ -111,8 +108,7 @@ class _RibbonPainter extends CustomPainter {
       nearLength = temp;
     }
     if (farLength > size.width) farLength = size.width;
-    TextSpan span =
-        TextSpan(style: titleStyle, text: title);
+    TextSpan span = TextSpan(style: titleStyle, text: title);
     textPainter = TextPainter(
         text: span,
         textAlign: TextAlign.center,
@@ -293,11 +289,10 @@ class _RibbonPainter extends CustomPainter {
       case RibbonLocation.bottomEnd:
         return -math.pi / 4;
     }
-    return 0;
   }
 
   Offset _center(List<Offset> vecs) {
-    double sum_x = 0, sum_y = 0, sum_s = 0;
+    double sumX = 0, sumY = 0, sumS = 0;
     double x1 = vecs[0].dx;
     double y1 = vecs[0].dy;
     double x2 = vecs[1].dx;
@@ -307,14 +302,14 @@ class _RibbonPainter extends CustomPainter {
       x3 = vecs[i].dx;
       y3 = vecs[i].dy;
       double s = ((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)) / 2.0;
-      sum_x += (x1 + x2 + x3) * s;
-      sum_y += (y1 + y2 + y3) * s;
-      sum_s += s;
+      sumX += (x1 + x2 + x3) * s;
+      sumY += (y1 + y2 + y3) * s;
+      sumS += s;
       x2 = x3;
       y2 = y3;
     }
-    double cx = sum_x / sum_s / 3.0;
-    double cy = sum_y / sum_s / 3.0;
+    double cx = sumX / sumS / 3.0;
+    double cy = sumY / sumS / 3.0;
     return Offset(cx, cy);
   }
 }
